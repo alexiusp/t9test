183f55d240ead3bd161324050284eae2a236232a
Aleksei "AlexiuS" Podgaev <podgaev@gmail.com>
2016-06-10 00:07:07 +0200

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+		<title>T9 Test</title>
+	</head>
+	<body>
+		T9 Test
+	</body>
+</html>
