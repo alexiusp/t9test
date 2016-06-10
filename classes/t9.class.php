40d512bb2a3133e15df3beb83ce81b979c2875a5
Aleksei "AlexiuS" Podgaev <podgaev@gmail.com>
2016-06-10 21:23:08 +0200

    final
    searching done
    refactoring done
    phpdoc done

--- classes/t9.class.php
+++ classes/t9.class.php
@@ -1,14 +1,43 @@
 <?php
+/**
+ * Class implementing the storage of phone contacts with T9-like search
+ *
+ * The idea is to store an additional field in the database with "code" which
+ * represents all necessary fields "encoded" to numbers of the phone keyboard
+ * where these letters belongs to
+ *
+ * @package T9Contacts
+ * @author    Aleksei Podgaev
+ * @copyright 2016
+ */
+ namespace T9Contacts;
+
 class T9Class {
 	private $db;
 	public $error;
+
 	function __construct() {
-		$this->db = new mysqli("localhost", "mysql", "mysql", "t9test");
+		$this->db = new \mysqli("localhost", "mysql", "mysql", "t9test");
 		if ($mysqli->connect_errno) {
 			$this->error = "Die Verbindung mit DB wurde nicht erstellt: " . $mysqli->connect_error;
 		} else $this->error = null;
    }
+
+	 /**
+	  * Encoding function
+		*
+		* encodes all characters
+		* in a given string to phone keys
+	  *
+	  * @param     string $inputStr string to encode
+	  * @return    string
+	  */
 	 private function encode($inputStr) {
+		 //
+		 /**
+		  * remove spaces and slashes
+			* may be we should remove other symbols like "-" too
+		  */
 		 $word = str_replace(' ', '', $inputStr);
 		 $word = str_replace('\'', '', $inputStr);
 		 $word = preg_replace('/[a-c]/i', '2', $word);
@@ -21,23 +50,56 @@ class T9Class {
 		 $word = preg_replace('/[w-z]/i', '9', $word);
 		 return $word;
 	 }
+
+	/**
+	 * Adds contact to the database
+	 *
+	 * @param     string $name Surname of the contact
+	 * @param     string $vorname First name of the contact
+	 * @param     string $tel optional phone number
+	 * @return    string
+	 */
 	 public function addContact($name, $vorname, $tel = "") {
+		 /**
+		  * resulting string
+		  */
 		 $code = "";
+		 /**
+		  * Escape and quote input parameters and encode them
+		  *
+		  * @todo implement proper validation of input and remove escaping
+		  */
 		 $n = '\''.$this->db->real_escape_string($name).'\'';
 		 $code .= $this->encode($n);
 		 $v = '\''.$this->db->real_escape_string($vorname).'\'';
 		 $code .= $this->encode($v);
 		 $t = (empty($tel))? 'NULL' : '\''.$this->db->real_escape_string($tel).'\'';
 		 if(!empty($tel)) $code .= $this->encode($t);
+		 /**
+		  * May be it has sense to add unencoded strings to the indexed field
+		  * so we can make more precise search for names
+		  */
 		 $code = '\''.$code.'\'';
-		 $result = $this->db->query("insert into contacts(name, vorname, tel, code) values ($n, $v, $t, $code)");
-		 if($result === false) $this->error = $this->db->error;
-		 return $result;
+		 $result = $this->db->query("insert into `contacts` (`name`, `vorname`, `tel`, `code`) values ($n, $v, $t, $code)");
+		 if($result === false) return $this->db->error;
+		 return "Der eingegebene Kontakt wurde erfolgreich hinzugefügt";
 	 }
+
+	 /**
+	  * Searches the database for a given string
+		*
+		* converts given string to a "code",
+		* then searches for a substring in a field "code"
+	  *
+	  * @param     string $word search keyword given by the user
+	  * @return    mixed
+	  */
 	 public function search($word) {
 		 $code = $this->encode($this->db->real_escape_string($word));
 		 $codePattern = '\'%'.$code.'%\'';
-		 $result = $this->db->query("select (name, vorname, tel) from contacts where code like $codePattern");
+		 $result = $this->db->query("select `name`, `vorname`, `tel` from `contacts` where `code` like $codePattern");
+		 if($result === false) $this->error = $this->db->error;
+		 return $result;
 	 }
 
 }
--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Main entry of application
+ *
+ * @package T9Contacts
+ * @author    Aleksei Podgaev
+ * @copyright 2016
+ */
+ namespace T9Contacts;
+
+/**
+ * include T9Class
+ */
+ require_once('/classes/t9.class.php');
+
+ /**
+  * "action" - defines what should we do
+  */
 $act = (array_key_exists('act', $_GET))? $_GET['act'] : 'index';
 ?>
 <!DOCTYPE html>
@@ -9,32 +26,60 @@ $act = (array_key_exists('act', $_GET))? $_GET['act'] : 'index';
 	</head>
 	<body>
 		<?php
+			// decide which function to call
+			// "router" in terms of MVC
 			switch ($act) {
-				case 'new':
-					require_once('/templates/add.form.html');
-					break;
 				case 'add':
-					require_once('/classes/t9.class.php');
 					$result = "";
-					try {
-						$t9 = new T9Class();
-						if(isset($t9->error)) throw new Exception($t9->error, 1);
-						$name = $_POST['name'];
-						$vorname = $_POST['vorname'];
-						$tel = $_POST['tel'];
-						if(empty($name) || empty($vorname)) throw new Exception("Wrong parameters", 1);
-						if($t9->addContact($name, $vorname, $tel) === false) {
-							$result = $t9->error;
-						} else {
-							$result = "OK";
+					// if post is filled with data we should add a new contact
+					if(is_array($_POST) && array_key_exists('name', $_POST)) {
+						try {
+							$t9 = new T9Class();
+							if(isset($t9->error))
+								throw new Exception("Verbindungfehler: ".$t9->error, 1);
+							/**
+							 * @todo it would be better to implement some validation
+							 */
+							$name = $_POST['name'];
+							$vorname = $_POST['vorname'];
+							$tel = $_POST['tel'];
+							if(empty($name) || empty($vorname))
+								throw new Exception("Falsche Eingaben", 1);
+							$result = $t9->addContact($name, $vorname, $tel);
+						} catch (Exception $e) {
+							$result = $e->getMessage();
 						}
-					} catch (Exception $e) {
-						$result = $e->message;
 					}
+					// show form for adding new contact
 					require_once('/templates/add.form.html');
 					break;
 				case 'search':
-					
+					$result = null;
+					$error = "";
+					$word = "";
+					// if POST is filled with data we should make a search
+					// for given keyword in a database
+					if(is_array($_POST) && array_key_exists('word', $_POST)) {
+						try {
+							$t9 = new T9Class();
+							if(isset($t9->error))
+								throw new Exception("Verbindungfehler: ".$t9->error, 1);
+							/**
+							 * @todo it would be better to implement some validation
+							 */
+							$word = $_POST['word'];
+							$result = $t9->search($word);
+							if($result === false) {
+								$error = "DB sagt: ".$t9->error;
+							} else {
+								require_once('/templates/result.html');
+							}
+						} catch (Exception $e) {
+							$error = "PHP sagt: ".$e->getMessage();
+						}
+					}
+					require_once('/templates/search.html');
+					break;
 				default:
 					require_once('/templates/index.html');
 					break;
