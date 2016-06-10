c38d06ff1a2c36b589a4cde59b1e5362461798f6
Aleksei Podgaev <podgaev@gmail.com>
2016-06-10 18:16:10 +0200

    almost finished
    encoding OK
    searching started

--- classes/t9.class.php
+++ classes/t9.class.php
@@ -1,12 +1,44 @@
 <?php
 class T9Class {
 	private $db;
+	public $error;
 	function __construct() {
 		$this->db = new mysqli("localhost", "mysql", "mysql", "t9test");
 		if ($mysqli->connect_errno) {
-			echo "Die Verbindung mit DB wurde nicht erstellt: " . $mysqli->connect_error;
-		}
-		echo "OK";
+			$this->error = "Die Verbindung mit DB wurde nicht erstellt: " . $mysqli->connect_error;
+		} else $this->error = null;
    }
+	 private function encode($inputStr) {
+		 $word = str_replace(' ', '', $inputStr);
+		 $word = str_replace('\'', '', $inputStr);
+		 $word = preg_replace('/[a-c]/i', '2', $word);
+		 $word = preg_replace('/[d-f]/i', '3', $word);
+		 $word = preg_replace('/[g-i]/i', '4', $word);
+		 $word = preg_replace('/[j-l]/i', '5', $word);
+		 $word = preg_replace('/[m-o]/i', '6', $word);
+		 $word = preg_replace('/[p-s]/i', '7', $word);
+		 $word = preg_replace('/[t-v]/i', '8', $word);
+		 $word = preg_replace('/[w-z]/i', '9', $word);
+		 return $word;
+	 }
+	 public function addContact($name, $vorname, $tel = "") {
+		 $code = "";
+		 $n = '\''.$this->db->real_escape_string($name).'\'';
+		 $code .= $this->encode($n);
+		 $v = '\''.$this->db->real_escape_string($vorname).'\'';
+		 $code .= $this->encode($v);
+		 $t = (empty($tel))? 'NULL' : '\''.$this->db->real_escape_string($tel).'\'';
+		 if(!empty($tel)) $code .= $this->encode($t);
+		 $code = '\''.$code.'\'';
+		 $result = $this->db->query("insert into contacts(name, vorname, tel, code) values ($n, $v, $t, $code)");
+		 if($result === false) $this->error = $this->db->error;
+		 return $result;
+	 }
+	 public function search($word) {
+		 $code = $this->encode($this->db->real_escape_string($word));
+		 $codePattern = '\'%'.$code.'%\'';
+		 $result = $this->db->query("select (name, vorname, tel) from contacts where code like $codePattern");
+	 }
+
 }
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $act = (array_key_exists('act', $_GET))? $_GET['act'] : 'index';
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<title>T9 Test</title>
 	</head>
 	<body>
@@ -15,8 +15,26 @@ $act = (array_key_exists('act', $_GET))? $_GET['act'] : 'index';
 					break;
 				case 'add':
 					require_once('/classes/t9.class.php');
-					$t9 = new T9Class();
+					$result = "";
+					try {
+						$t9 = new T9Class();
+						if(isset($t9->error)) throw new Exception($t9->error, 1);
+						$name = $_POST['name'];
+						$vorname = $_POST['vorname'];
+						$tel = $_POST['tel'];
+						if(empty($name) || empty($vorname)) throw new Exception("Wrong parameters", 1);
+						if($t9->addContact($name, $vorname, $tel) === false) {
+							$result = $t9->error;
+						} else {
+							$result = "OK";
+						}
+					} catch (Exception $e) {
+						$result = $e->message;
+					}
+					require_once('/templates/add.form.html');
 					break;
+				case 'search':
+					
 				default:
 					require_once('/templates/index.html');
 					break;
